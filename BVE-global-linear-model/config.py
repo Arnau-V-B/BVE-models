e2037b6c6793e07f63a426975c83633344146ebd
Arnau-V-B <233453020+Arnau-V-B@users.noreply.github.com>
2026-07-25 19:43:03 +0200

    Adapted the code in 'main_BVE_glob_linear.py' to import the 'plotting.py' module when generating the figures

--- BVE-global-linear-model/config.py
+++ BVE-global-linear-model/config.py
@@ -36,7 +36,8 @@ alpha = 0.5        									# Displacement factor
 
 # OUTPUT ====================================================================================================
 output_name = "exp_test"       						# Name of the output folder
-save_time = 6 * 3600         						# Time interval between saves (in s) 
+save_time = 6 * 3600         						# Time interval between saves (in s)
+PLOT = False         								# Switch on/off plotting
 
 
 # INITIAL CONDITIONS ========================================================================================
